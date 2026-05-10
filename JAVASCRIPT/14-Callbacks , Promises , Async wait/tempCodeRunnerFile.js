ccf343d7d8782e276422c9e1083354b3c4433492
Jawad Arshad <jawadarshad.dev@gmail.com>
2026-05-10 23:25:37 +0500

    In-depth revision of Callback Hell with examples

--- JAVASCRIPT/14-Callbacks , Promises , Async wait/tempCodeRunnerFile.js
+++ JAVASCRIPT/14-Callbacks , Promises , Async wait/tempCodeRunnerFile.js
@@ -1,35 +1,60 @@
-function getdata3(dataid){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("Data with id " + dataid + " is fetched");
-            resolve("Success");
-        }, 2000);
+
+// Online Food Order System (Callback hell Exampe)
+
+    let orderDetail = {
+        orderId : 12345,
+        food : ["Pizza", "Burger", "Coke"],
+        cost : 500,
+        customer_name: "Jawad",
+        customer_location: "Lahore",
+        restaurant_location: "Karachi"
+    }
+
+
+    function placeOrder(orderDetail, callbackFunc){
+        console.log(`${orderDetail.cost} Payment is in progress`)
+        setTimeout(()=>{
+            console.log("Payment is recieved and order gets placed")
+            orderDetail.status = true;
+            callbackFunc(orderDetail);
+        
+        },3000)
+    }
+
+    function prepareingOrder(orderDetail , callbackFunc){
+        console.log(`Your food preparation started ${orderDetail.food}`)
+        setTimeout(()=>{
+            console.log("Your order is now prepared")
+            orderDetail.token = 123;
+            callbackFunc(orderDetail);
+        },3000)
+    }
+
+    function pickupOrder(orderDetail , callbackFunc){
+        console.log(`Delivery boy is on the way to pickup Order from ${orderDetail.restaurant_location}` )   
+
+        setTimeout(()=>{
+            console.log("I have picked up the Order")
+            orderDetail.recieved = true;    
+            callbackFunc(orderDetail);
+        },3000)
+    }
+
+    function delieverOrder(orderDetail){
+        console.log(`I am on the way to deliver the Order to ${orderDetail.customer_location}` )
+
+        setTimeout(()=>{
+            console.log("Order Deliever Successfully")
+            orderDetail.delievery = true;
+        },3000)
+    }
+
+
+// Callback hell
+    placeOrder(orderDetail , (orderDetail)=>{
+        prepareingOrder(orderDetail , (orderDetail)=>{
+            pickupOrder(orderDetail , (orderDetail)=>{
+                delieverOrder(orderDetail)
+            })
+        });
     });
-}
-
-//  async function getAllDatawithIDs(){
-//     console.log("Getting data 1....")
-//     await getdata3(1);
-//     console.log("Getting data 2....")
-//     await getdata3(2);
-//     console.log("Getting data 3....")
-//     await getdata3(3);
-//     console.log("Getting data 4....")
-//     await getdata3(4);
-// }
-// getAllDatawithIDs()
-
-
-//IIFE = Immediately Invoked Function Expression
-// immediately invoked function expression
-// syntax: (function(){})();
- (async function (){
-    console.log("Getting data 1....")
-    await getdata3(1);
-    console.log("Getting data 2....")
-    await getdata3(2);
-    console.log("Getting data 3....")
-    await getdata3(3);
-    console.log("Getting data 4....")
-    await getdata3(4);
-})();
