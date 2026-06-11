6f71329bf3e96a3e1985616ea06d7e904dc016e8
Jawad Arshad <jawadarshad.dev@gmail.com>
2026-06-11 22:35:57 +0500

    Added useEffect hook practice and examples

--- Reactjs/React CoderArmy/05-useEffect Hook/vite-project/src/Clock.jsx
+++ Reactjs/React CoderArmy/05-useEffect Hook/vite-project/src/Clock.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 function Clock(){
     const [time, setTime] = useState(new Date().toLocaleTimeString());
+    const [checkButton, setCheckButton] = useState(true)
 
 
 
@@ -14,16 +15,29 @@ function Clock(){
 
    // Solution is useEffect hook , jo ka setInterval ko webApi ko sirf aik hi dafa da ga
    useEffect(()=>{
-    setInterval(()=>{
+
+    if (!checkButton)
+         return; // agar checkButton false hai to useEffect ko run nahi karna hai.
+
+    const intervalId =  setInterval(()=>{
     setTime(new Date().toLocaleTimeString());
     console.log("Checking")
    },1000)
 
-   },[])
+// ya return checkButton change hone sa pahle run hoga.
+   return ()=>{
+    clearInterval(intervalId);
+   }
+
+   },[checkButton]) // jab bhi checkButton change hoga tabhi useEffect run hoga.
    
     return (
         <>
-        <h1>Current Time: {time}</h1>
+        <button onClick={()=>setCheckButton(!checkButton)}>{checkButton? "Hide": "Show"}</button>
+        {
+            checkButton && <h1>Current Time: {time}</h1>
+        }
+        
         </>
     )
 }
